184032313ebaa116d0a70180767c4f945505bc75
Thom Smith <thom.smith@spektrix.com>
2019-12-02 17:50:52 +0000

    day-01 - part2

--- day01/day01.ps1
+++ day01/day01.ps1
@@ -2,11 +2,48 @@
 
 #$modules = @(12, 14, 1969,100756)
 
-$fuel = 0
-foreach($module in $modules)
-{
-    $result = [System.Math]::Floor($module / 3) - 2
-    $fuel = $fuel + $result
-    Write-Host "Module mass: $module, fuel needed: $result, running total fuel: $fuel"
+function CalculateFuel{
+    Param(
+        [int]$mass
+    )
+
+    $result = [System.Math]::Floor($mass / 3) - 2
+
+    if($result -lt 0)
+    {
+        $result = 0
+    }
+    return $result
 }
-Write-Host "Total Fuel required: $fuel"
+
+function CalculateExtraFuel{
+    Param(
+        [int]$fuel
+    )
+
+    [int[]]$extraFuels =@()
+    while($true)
+    {
+        $fuel = CalculateFuel -mass $fuel
+        
+        if($fuel -le 0)
+        {
+            $totalExtra = 0
+            $extraFuels | %{$totalExtra += $_}
+            return $totalExtra
+            break
+        }
+        $extraFuels += $fuel
+    }
+}
+
+$totalFuel = 0
+foreach($moduleMass in $modules)
+{
+    $moduleFuel = CalculateFuel -mass $moduleMass
+    $extraFuel = CalculateExtraFuel -fuel $moduleFuel    
+
+    $totalFuel += ($moduleFuel + $extraFuel)
+    Write-Host "Module mass: $moduleMass, fuel needed: $moduleFuel, extra fuel needed: $extraFuel, running total fuel: $totalFuel"
+}
+Write-Host "Total Fuel required: $totalFuel"
