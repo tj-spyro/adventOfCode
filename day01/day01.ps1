eea4b80475521a3c66e37dd4fe245794ac7b8f4a
Thom Smith <thom.smith@spektrix.com>
2019-12-02 17:08:36 +0000

    day01 - part1

---
+++ day01/day01.ps1
@@ -0,0 +1,12 @@
+﻿[int[]]$modules = Get-Content -Path .\input.txt
+
+#$modules = @(12, 14, 1969,100756)
+
+$fuel = 0
+foreach($module in $modules)
+{
+    $result = [System.Math]::Floor($module / 3) - 2
+    $fuel = $fuel + $result
+    Write-Host "Module mass: $module, fuel needed: $result, running total fuel: $fuel"
+}
+Write-Host "Total Fuel required: $fuel"
