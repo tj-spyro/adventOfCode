6ecf36bc2e4502cd7aebaac06a16d845d15e19d8
Thom Smith <thom.smith@spektrix.com>
2019-12-02 21:40:44 +0000

    day01 -part 1 in c#

---
+++ SantasSpacecraft/day01/FuelCounterUpper.Tests/UnitTest.cs
@@ -0,0 +1,49 @@
+using System.Linq;
+using NUnit.Framework;
+
+namespace day01.Tests
+{
+    [TestFixture]
+    public class When_calculating_fuel_for_a_module
+    {
+        [TestCase(12, ExpectedResult = 2)]
+        [TestCase(14, ExpectedResult = 2)]
+        [TestCase(1969, ExpectedResult = 654)]
+        [TestCase(100756, ExpectedResult = 33583)]
+        public decimal Then_the_correct_fuel_amount_is_returned(int mass)
+        {
+            var sut = new FuelCounterUpper();
+            return sut.CalculateFuel(mass);
+        }
+    }
+
+    [TestFixture]
+    public class When_calculating_fuel_for_multiple_modules
+    {
+        [Test]
+        public void Then_the_correct_total_is_returned()
+        {
+            var modules = new int[] { 12, 14, 1969, 100756 };
+            var expected = new[] { 2, 2, 654, 33583 }.Sum();
+
+            var sut = new FuelCounterUpper();
+            var result = sut.CalculateFuel(modules);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+
+    public class When_calculating_all_modules_fuel
+    {
+        [Test]
+        public void Then_we_get_the_total_fuel()
+        {
+            var testFilePath = "..//..//..//TestData//input.txt";
+
+            var expected = 3394032;
+
+            var sut = new FuelCounterUpper();
+            var result = sut.CalculateFuelFromFile(testFilePath);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ SantasSpacecraft/day01/FuelCounterUpper/FuelCounterUpper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day01
+{
+    public class FuelCounterUpper
+    {
+        public int CalculateFuel(int mass)
+        {
+            return (int) Math.Floor((decimal)mass / 3) - 2;
+        }
+
+        public int CalculateFuel(IEnumerable<int> modules)
+        {
+            return modules.Sum(CalculateFuel);
+        }
+
+        public int CalculateFuelFromFile(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+
+            var modules = lines.Select(l => int.Parse(l));
+
+            return CalculateFuel(modules);
+        }
+    }
+}
