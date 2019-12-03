46498d59f4cb14cc33b4174b8f52fc21e3ebb647
Thom Smith <thom.smith@spektrix.com>
2019-12-03 00:06:31 +0000

    Day02 - part 1

---
+++ SantasSpacecraft/day02/Intcode.Tests/UnitTest.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Intcode.Tests
+{
+    public class When_handling_oppcode_1_add
+	{
+        [Test]
+        public void Then_the_numbers_are_added()
+		{
+			var testString = "1,9,10,3,2,3,11,0,99,30,40,50";
+
+			var sut = new Intcode(testString);
+
+			sut.ProcessState();
+
+			var result = sut.State[3];
+			var expected = 70;
+
+			Assert.That(result, Is.EqualTo(expected));
+		}
+	}
+
+	public class When_handling_oppcode_2_multiply
+	{
+		[Test]
+		public void Then_the_numbers_are_multiplied()
+		{
+			var testString = "1,9,10,3,2,3,11,0,99,30,40,50";
+
+			var sut = new Intcode(testString);
+
+			sut.ProcessState();
+
+			var result = sut.State[0];
+			var expected = 3500;
+
+			Assert.That(result, Is.EqualTo(expected));
+		}
+	}
+
+    public class StateTests
+	{
+		[TestCase("1,0,0,0,99", "2,0,0,0,99")]
+		[TestCase("2,3,0,3,99", "2,3,0,6,99")]
+		[TestCase("2,4,4,5,99,0", "2,4,4,5,99,9801")]
+        [TestCase("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")]
+        public void StatesAreUpdated(string intitialState, string expectedEndState)
+		{
+			var sut = new Intcode(intitialState);
+			sut.ProcessState();
+			var result = string.Join(",", sut.State);
+
+			Assert.That(result, Is.EqualTo(expectedEndState));
+		}
+	}
+
+    public class When_processing_the_test_state
+    {
+        private Intcode _intCode;
+
+        [SetUp]
+        public void Setup()
+        {
+            var testFilePath = "..//..//..//TestData//input.txt";
+
+            var initialState = File.ReadLines(testFilePath).First();
+
+            _intCode = new Intcode(initialState);
+
+            _intCode.ResetState();
+            _intCode.ProcessState();
+        }
+
+        [Test]
+        public void Then_result_at_position_zero_is_correct()
+        {
+            var result = _intCode.State[0];
+
+            Assert.That(result, Is.EqualTo(5110675));
+        }
+    }
+}
---
+++ SantasSpacecraft/day02/Intcode/Intcode.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace Intcode
+{
+    public class Intcode
+    {
+        public Intcode(string str)
+        {
+            State = ConvertStringToState(str);
+        }
+
+        public int[] State;
+
+        public void ProcessState()
+        {
+            var i = 0;
+            var continueProcessing = true;
+            while (continueProcessing)
+            {
+                continueProcessing = HandleSection(i);
+                i += 4;
+            }
+        }
+
+        public int[] ConvertStringToState(string str)
+        {
+            return str.Split(",").Select(s => int.Parse(s.Trim())).ToArray();
+        }
+
+        public void ResetState()
+        {
+            State[1] = 12;
+            State[2] = 2;
+        }
+
+        public bool HandleSection(int startPosition)
+        {
+            var oppCode = (OppCode)State[startPosition];
+
+            if (oppCode == OppCode.Terminate)
+            {
+                return false;
+            }
+
+            var inputPosition1 = State[startPosition + 1];
+            var inputPosition2 = State[startPosition + 2];
+            var outputPosition = State[startPosition + 3];
+
+            var inputValue1 = State[inputPosition1];
+            var inputValue2 = State[inputPosition2];
+
+            int result;
+            switch (oppCode)
+            {
+                case OppCode.Add:
+                    result = Add(inputValue1, inputValue2);
+                    break;
+                case OppCode.Multiply:
+                    result = Multiply(inputValue1, inputValue2);
+                    break;
+                case OppCode.Terminate:
+                    return false;
+                default:
+                    throw new ApplicationException();
+            }
+
+            State[outputPosition] = result;
+
+            return true;
+        }
+
+        public int Add(int input1, int input2)
+        {
+            return input1 + input2;
+        }
+
+        public int Multiply(int input1, int input2)
+        {
+            return input1 * input2;
+        }
+    }
+
+    public enum OppCode
+    {
+        Add = 1,
+        Multiply,
+        Terminate = 99
+    }
+}
