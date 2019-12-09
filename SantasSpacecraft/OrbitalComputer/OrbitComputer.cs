b762c56cef211ff234b34e34640848c96ae3ee3c
Thom Smith <thom.smith@spektrix.com>
2019-12-09 12:18:40 +0000

    day06 - part1

---
+++ SantasSpacecraft/OrbitalComputer/OrbitComputer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrbitalComputer
+{
+    public class OrbitComputer
+    {
+        private readonly string[] _input;
+
+        private const string OrbitDelimiter = ")";
+
+        private const string UniversalCentreOfMass = "COM";
+
+        private Dictionary<string, (string direct, List<string> indirect)> orbits;
+
+        public OrbitComputer(string[] input)
+        {
+            _input = input;
+        }
+
+        public void Initialise()
+        {
+            if (orbits != null)
+            {
+                return;
+            }
+            orbits = new Dictionary<string, (string direct, List<string> inDirect)>();
+            foreach (var s in _input)
+            {
+                var orbiter = s.Substring(s.IndexOf(OrbitDelimiter, StringComparison.Ordinal) + 1);
+                var orbitee = s.Substring(0, s.IndexOf(OrbitDelimiter, StringComparison.Ordinal));
+
+                orbits.Add(orbiter, (orbitee, new List<string>()));
+            }
+        }
+
+        public int CalculateMapDataChecksum()
+        {
+            Initialise();
+
+            foreach (var orbit in orbits)
+            {
+                PopulateIndirectOrbits(orbit.Key);
+            }
+
+            return orbits.Sum(o => o.Value.indirect.Count);
+        }
+
+        public List<string> PopulateIndirectOrbits(string orbiter)
+        {
+            if (orbiter == UniversalCentreOfMass)
+            {
+                return new List<string>();
+            }
+
+            var indirects = orbits[orbiter].indirect;
+
+            for (var o = orbiter; orbits.ContainsKey(o); o = orbits[o].direct)
+            {
+                indirects.Add(o);
+            }
+
+            return indirects;
+        }
+    }
+}
---
+++ SantasSpacecraft/SantasSpacecraft.Tests/day06.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Linq;
+using Intcode;
+using Intcode.Memory;
+using NUnit.Framework;
+using OrbitalComputer;
+
+namespace SantasSpacecraft.Tests.Day06
+{
+    public class When_populating_orbits
+    {
+        private readonly string[] _testInput = new[]
+            {"COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"};
+        
+        [TestCase("D", ExpectedResult = 3)]
+        [TestCase("L", ExpectedResult = 7)]
+        [TestCase("COM", ExpectedResult = 0)]
+        public int Then_count_is_correct(string orbiter)
+        {
+            var orbitComputer = new OrbitComputer(_testInput);
+            orbitComputer.Initialise();
+
+            return orbitComputer.PopulateIndirectOrbits(orbiter).Count;
+        }
+    }
+
+    [TestFixture]
+    public class Solve
+    {
+        private string[] _testInput;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            const string testFilePath = "..//..//..//TestData//day06.txt";
+
+            _testInput = File.ReadLines(testFilePath).ToArray();
+        }
+
+        [Test]
+        public void Part_1()
+        {
+            var orbitComputer = new OrbitComputer(_testInput);
+            orbitComputer.Initialise();
+
+            var result = orbitComputer.CalculateMapDataChecksum();
+
+            Assert.That(result, Is.EqualTo(453028));
+        }
+
+        [Test]
+        public void Part_2()
+        {
+            
+        }
+    }
+}
